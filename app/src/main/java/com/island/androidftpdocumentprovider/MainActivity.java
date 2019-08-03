3d87cd956348a891cd542b937cfe45e2d37a90c3
RikyIsola <riky.isola@gmail.com>
2019-08-03 23:18:05 +0200

    Initial commit

---
+++ app/src/main/java/com/island/androidftpdocumentprovider/MainActivity.java
@@ -0,0 +1,13 @@
+package com.island.androidftpdocumentprovider;
+import android.app.*;
+import android.os.Bundle;
+public class MainActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}

d4723bb443b3e0315f8f2017bce649d51efa7f15
RikyIsola <riky.isola@gmail.com>
2019-08-12 16:35:00 +0200

    Add directories and open files

--- app/build/gen/com/island/androidftpdocumentprovider/R.java
+++ app/build/gen/com/island/androidftpdocumentprovider/R.java
@@ -11,31 +11,27 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int colorAccent=0x7f050002;
-        public static final int colorPrimary=0x7f050000;
-        public static final int colorPrimaryDark=0x7f050001;
+        public static final int colorAccent=0x7f040002;
+        public static final int colorPrimary=0x7f040000;
+        public static final int colorPrimaryDark=0x7f040001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int ic_launcher_round=0x7f020001;
     }
     public static final class id {
-        public static final int ip=0x7f080000;
-        public static final int port=0x7f080001;
+        public static final int ip=0x7f070000;
+        public static final int port=0x7f070001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f040000;
-    }
-    public static final class mipmap {
-        public static final int ic_launcher=0x7f030000;
-        public static final int ic_launcher_round=0x7f030001;
+        public static final int activity_main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f060000;
+        public static final int app_name=0x7f050000;
     }
     public static final class style {
         /**  Base application theme. 
          */
-        public static final int AppTheme=0x7f070000;
+        public static final int AppTheme=0x7f060000;
     }
 }
--- app/src/main/java/com/island/androidftpdocumentprovider/FtpProvider.java
+++ app/src/main/java/com/island/androidftpdocumentprovider/FtpProvider.java
@@ -8,167 +8,200 @@ import android.webkit.*;
 import java.io.*;
 import com.enterprisedt.net.ftp.*;
 import android.util.*;
+import java.util.*;
+import java.text.*;
 public class FtpProvider extends DocumentsProvider
 {
 	private static final String[] DEFAULT_ROOT_PROJECTION =
 	new String[]{Root.COLUMN_ROOT_ID,
         Root.COLUMN_FLAGS, Root.COLUMN_ICON, Root.COLUMN_TITLE,
-        Root.COLUMN_SUMMARY, Root.COLUMN_DOCUMENT_ID};
+        //Root.COLUMN_SUMMARY, 
+		Root.COLUMN_DOCUMENT_ID};
 	private static final String[] DEFAULT_DOCUMENT_PROJECTION = new
-	String[]{Document.COLUMN_DOCUMENT_ID, Document.COLUMN_MIME_TYPE,
-        Document.COLUMN_DISPLAY_NAME, Document.COLUMN_LAST_MODIFIED,
-        Document.COLUMN_FLAGS, Document.COLUMN_SIZE};
-	private FTPClient client;
+	String[]{Document.COLUMN_DOCUMENT_ID,Document.COLUMN_SIZE,
+        Document.COLUMN_DISPLAY_NAME,Document.COLUMN_LAST_MODIFIED,Document.COLUMN_MIME_TYPE,
+        Document.COLUMN_FLAGS};
+	String[]connections={"127.0.0.1:8888"};
 	@Override
 	public boolean onCreate()
 	{
-		StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().build());
+		/*StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().build());
 		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().build());
 		SharedPreferences preferences=getContext().getSharedPreferences(MainActivity.CONFIG,0);
 		try
 		{
 			client=new FTPClient(preferences.getString(MainActivity.PORT,"127.0.0.1"),preferences.getInt(MainActivity.PORT,8888));
+			client.login("anonymous","anonymous@anonymous.com");
 			Log.i(MainActivity.LOG_TAG,client.toString());
 			return true;
 		}
 		catch(FTPException e)
 		{
-			throw new RuntimeException(e);
+			Log.e(MainActivity.LOG_TAG,"Query child exception",e);
 		}
 		catch(IOException e)
 		{
-			throw new RuntimeException(e);
-		}
+			Log.e(MainActivity.LOG_TAG,"Query child exception",e);
+		}*/
+		return true;
 	}
 	@Override
 	public Cursor queryRoots(String[]projection)throws FileNotFoundException
 	{
-		Log.i(MainActivity.LOG_TAG,"query root");
+		Log.i(MainActivity.LOG_TAG,"Query Root: Projection="+Arrays.toString(projection));
 		MatrixCursor result=new MatrixCursor(projection!=null?projection:DEFAULT_ROOT_PROJECTION);
-		MatrixCursor.RowBuilder row=result.newRow();
-		row.add(Root.COLUMN_ROOT_ID,"127.0.0.1");
-		row.add(Root.COLUMN_DOCUMENT_ID,"127.0.0.1");
-		row.add(Root.COLUMN_SUMMARY, "ftp provider test");
-		row.add(Root.COLUMN_ICON, R.drawable.ic_launcher);
-		row.add(Root.COLUMN_FLAGS, Root.FLAG_SUPPORTS_CREATE);
-		row.add(Root.COLUMN_TITLE,"ftp");
-		
+		for(String connection:connections)
+		{
+			MatrixCursor.RowBuilder row=result.newRow();
+			row.add(Root.COLUMN_ROOT_ID,connection);
+			row.add(Root.COLUMN_DOCUMENT_ID,connection+"/");
+			//row.add(Root.COLUMN_SUMMARY, "ftp provider test");
+			row.add(Root.COLUMN_ICON, R.drawable.ic_launcher);
+			row.add(Root.COLUMN_FLAGS, Root.FLAG_SUPPORTS_CREATE);
+			row.add(Root.COLUMN_TITLE,connection);
+		}
 		return result;
 	}
 	@Override
-	public Cursor queryDocument(String documentId,String[]projection) throws FileNotFoundException
+	public Cursor queryDocument(String documentId,String[]projection)throws FileNotFoundException
 	{
-		Log.i(MainActivity.LOG_TAG,"query document");
+		Log.i(MainActivity.LOG_TAG,"Query Document: DocumentId="+documentId+" Projection="+Arrays.toString(projection));
 		MatrixCursor result=new MatrixCursor(projection!=null?projection:DEFAULT_DOCUMENT_PROJECTION);
-		String[][]resolved=resolveProjection(documentId,projection);
-		for(String[]resolve:resolved)result.addRow(resolve);
-		return result;
-	}
-	@Override
-	public Cursor queryChildDocuments(String documentId,String[]projection,String sortOrder)throws FileNotFoundException
-	{
-		Log.i(MainActivity.LOG_TAG,"query documents");
-		MatrixCursor result=new MatrixCursor(projection!=null?projection:DEFAULT_DOCUMENT_PROJECTION);
-		String[][]resolved=resolveProjection(documentId,projection);
-		for(String[]resolve:resolved)result.addRow(resolve);
-		return result;
-	}
-	@Override
-	public ParcelFileDescriptor openDocument(String p1,String p2,CancellationSignal p3) throws FileNotFoundException
-	{
-		Log.i(MainActivity.LOG_TAG,p1.toString());
-		// TODO: Implement this method
-		return null;
-	}
-	public String[][]resolveProjection(String document,String[]projection)
-	{
+		if(documentId.equals("children"))return result;
 		try
 		{
-			String[]files=client.dir(document,true);
-			String[][]resolved=new String[files.length][projection.length];
-			for(int b=0;b<files.length;b++)
-			{
-				String file=files[b];
-				Log.i(MainActivity.LOG_TAG,file);
-				for(int a=0;a<projection.length;a++)
-				{
-					switch(projection[a])
-					{
-						case DocumentsContract.Document.COLUMN_DISPLAY_NAME:
-							resolved[b][a]=file;
-							break;
-						case DocumentsContract.Document.COLUMN_DOCUMENT_ID:
-							resolved[b][a]=document+file;
-							//if(file.contains("/"))resolved[b][a]+="/";
-							break;
-						case DocumentsContract.Document.COLUMN_FLAGS:
-							resolved[b][a]=String.valueOf(Document.FLAG_DIR_SUPPORTS_CREATE|Document.FLAG_SUPPORTS_COPY|Document.FLAG_SUPPORTS_DELETE|Document.FLAG_SUPPORTS_DELETE|Document.FLAG_SUPPORTS_MOVE|Document.FLAG_SUPPORTS_REMOVE|Document.FLAG_SUPPORTS_RENAME|Document.FLAG_SUPPORTS_WRITE);
-							break;
-						case DocumentsContract.Document.COLUMN_ICON:
-							resolved[b][a]=null;
-							break;
-						case DocumentsContract.Document.COLUMN_LAST_MODIFIED:
-							resolved[b][a]=null;
-							break;
-						case DocumentsContract.Document.COLUMN_MIME_TYPE:
-							resolved[b][a]=MimeTypeMap.getSingleton().getMimeTypeFromExtension(file.substring(file.lastIndexOf(".")));
-							break;
-						case DocumentsContract.Document.COLUMN_SIZE:
-							//resolved[b][a]=String.valueOf(file.getSize());
-							break;
-						case DocumentsContract.Document.COLUMN_SUMMARY:
-							resolved[b][a]=null;
-							break;
-					}
-				}
-			}
-			return resolved;
+			getFileInfo(documentId,"drwx------   0 anonymous anonymous            0 Aug 11 03:11 ",new Date(),result.newRow());
+			//return queryChildDocuments(documentId,projection,(String)null);
 		}
-		catch(FTPException e)
+		catch(Exception e)
 		{
-			throw new RuntimeException(e);
+			String msg="Error querying child "+getPath(documentId);
+			Log.e(MainActivity.LOG_TAG,msg,e);
+			throw new FileNotFoundException(msg);
 		}
-		catch(IOException e)
-		{
-			throw new RuntimeException(e);
-		}
+		return result;
 	}
-	public String[]resolveRootProjection(String[]projection)
+	@Override
+	public Cursor queryChildDocuments(String parentDocumentId,String[]projection,String sortOrder)throws FileNotFoundException
 	{
-		String[]resolved=new String[projection.length];
-		for(int a=0;a<projection.length;a++)
+		Log.i(MainActivity.LOG_TAG,"Query Child Documents: ParentDocumentId="+parentDocumentId+" Projection="+projection+" SortOrder="+sortOrder);
+		MatrixCursor result=new MatrixCursor(projection!=null?projection:DEFAULT_DOCUMENT_PROJECTION);
+		try
 		{
-			Log.i(MainActivity.LOG_TAG,projection[a]);
-			switch(projection[a])
+			if(parentDocumentId.charAt(parentDocumentId.length()-1)!='/')parentDocumentId+="/";
+			String[]protocolParameters=getRoot(parentDocumentId);
+			FTPClient client=new FTPClient(protocolParameters[0],Integer.valueOf(protocolParameters[1]));
+			client.login("anonymous","anonymous");
+			String path=getPath(parentDocumentId);
+			String[]filesDescription=client.dir(path,true);
+			String[]files=client.dir(path);
+			for(int a=0;a<filesDescription.length;a++)
 			{
-				case DocumentsContract.Root.COLUMN_AVAILABLE_BYTES:
-					resolved[a]=null;
-					break;
-				case DocumentsContract.Root.COLUMN_CAPACITY_BYTES:
-					resolved[a]=null;
-					break;
-				case DocumentsContract.Root.COLUMN_DOCUMENT_ID:
-					resolved[a]="127.0.0.1/";
-					break;
-				case DocumentsContract.Root.COLUMN_FLAGS:
-					resolved[a]=String.valueOf(DocumentsContract.Root.FLAG_SUPPORTS_CREATE);
-					break;
-				case DocumentsContract.Root.COLUMN_ICON:
-					resolved[a]=String.valueOf(R.mipmap.ic_launcher);
-					break;
-				case DocumentsContract.Root.COLUMN_MIME_TYPES:
-					resolved[a]=null;
-					break;
-				case DocumentsContract.Root.COLUMN_ROOT_ID:
-					resolved[a]="127.0.0.1";
-					break;
-				case DocumentsContract.Root.COLUMN_SUMMARY:
-					resolved[a]="Ftp test";
-					break;
-				case DocumentsContract.Root.COLUMN_TITLE:
-					resolved[a]="FTP";
+				getFileInfo(parentDocumentId+files[a],filesDescription[a],client.modtime(path+files[a]),result.newRow());
 			}
 		}
-		return resolved;
+		catch(Exception e)
+		{
+			String msg="Error querying childs of "+getPath(parentDocumentId);
+			Log.e(MainActivity.LOG_TAG,msg,e);
+			throw new FileNotFoundException(msg);
+		}
+		return result;
 	}
+	@Override
+	public ParcelFileDescriptor openDocument(final String documentId,String mode,CancellationSignal signal)throws FileNotFoundException
+	{
+		Log.i(MainActivity.LOG_TAG,"Open Document: DocumentId="+documentId+" mode="+mode+" signal="+signal);
+		final File file=new File(getContext().getExternalCacheDir(),"tmp");
+		int accessMode=ParcelFileDescriptor.parseMode(mode);
+		boolean isWrite=(mode.indexOf('w')!=-1);
+		String[]connectionParameter=getRoot(documentId);
+		try
+		{
+			final FTPClient client=new FTPClient(connectionParameter[0],Integer.valueOf(connectionParameter[1]));
+			client.login("anonymous","anonymous");
+			client.get(file.getPath(),getPath(documentId));
+			if(isWrite)
+			{
+				return ParcelFileDescriptor.open(file,accessMode,new Handler(getContext().getMainLooper()),new ParcelFileDescriptor.OnCloseListener()
+					{
+						@Override
+						public void onClose(IOException exception)
+						{
+							try
+							{
+								client.put(file.getPath(),getPath(documentId));
+							}
+							catch(Exception e)
+							{
+								Log.e(MainActivity.LOG_TAG,"Error opening document "+getPath(documentId),e);
+							}
+						}
+					});
+			}
+			else
+			{
+				return ParcelFileDescriptor.open(file,accessMode);
+			}
+		}
+		catch(Exception e)
+		{
+			String msg="Error opening document "+getPath(documentId);
+			Log.e(MainActivity.LOG_TAG,msg,e);
+			throw new FileNotFoundException(msg);
+		}
+	}
+	private static String[]getRoot(String documentId)
+	{
+		return documentId.substring(0,documentId.indexOf("/")).split(":");
+	}
+	private static String getPath(String documentId)
+	{
+		return documentId.substring(documentId.indexOf("/")+1);
+	}
+	private void getFileInfo(String documentId,String fileDetails,Date date,MatrixCursor.RowBuilder row)
+	{
+		int flags;
+		String mime;
+		Scanner scanner=new Scanner(fileDetails);
+		String permissions=scanner.next();
+		scanner.next();
+		scanner.next();
+		scanner.next();
+		long size=scanner.nextLong();
+		scanner.next();
+		scanner.next();
+		scanner.next();
+		String file=scanner.nextLine();
+		if(permissions.charAt(0)=='d')
+		{
+			Log.i(MainActivity.LOG_TAG,"directory found");
+			flags=Document.FLAG_DIR_SUPPORTS_CREATE;
+			mime=Document.MIME_TYPE_DIR;
+			if(file.equals(" "))file="";
+		}
+		else
+		{
+			flags=Document.FLAG_SUPPORTS_WRITE;
+			mime=getTypeForName(file);
+			row.add(Document.COLUMN_SIZE,size);
+		}
+		flags|=Document.FLAG_SUPPORTS_COPY|Document.FLAG_SUPPORTS_DELETE|Document.FLAG_SUPPORTS_MOVE|Document.FLAG_SUPPORTS_REMOVE|Document.FLAG_SUPPORTS_RENAME;
+		row.add(Document.COLUMN_FLAGS,flags);
+		row.add(Document.COLUMN_MIME_TYPE,mime);
+		row.add(Document.COLUMN_DISPLAY_NAME,file);
+		row.add(Document.COLUMN_DOCUMENT_ID,documentId);
+		row.add(Document.COLUMN_LAST_MODIFIED,date.getTime());
+	}
+	private static String getTypeForName(String name) {
+        final int lastDot = name.lastIndexOf('.');
+        if (lastDot >= 0) {
+            final String extension = name.substring(lastDot + 1);
+            final String mime = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+            if (mime != null) {
+                return mime;
+            }
+        }
+        return "application/octet-stream";
+    }
 }
